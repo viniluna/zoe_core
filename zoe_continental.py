48679ac823c319116ba055c39454ffac926dd6f3
Data Science <43393687+viniluna@users.noreply.github.com>
2024-09-23 22:52:21 +0000

    add build plan

--- zoe_continental.py
+++ zoe_continental.py
@@ -25,7 +25,7 @@ MENSAGENS = "mensagens"
 def obter_llm() -> ChatOpenAI:
     return ChatOpenAI(
         model_name='gpt-3.5-turbo',
-        openai_api_key='sk-a_clhEM-Ba75n3miFQcBGoaMGUpR46AgDb1dxI6NwDT3BlbkFJrgb_zL0sJ1P2X7X9fxVhs1inuFYT1dOokjdssVfXIA'
+        openai_api_key=st.secrets["openai_token"]
     )
 
 # Função para criar o LLMChain com memória de conversação
